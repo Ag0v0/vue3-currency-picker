ec12c2c756733c280ef47e121e6d3526ded4ef29
Ag <ag@ciglink.com>
2023-12-07 14:13:06 +0800

    ..

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 // import { CurrencyPicker } from '../dist/index.mjs'
 // import '../dist/style.css' // 导入样式
 
-const form = reactive({
-  currency: 'CNY'
-})
+const form = ref({})
+
+function init() {
+  form.value = {}
+}
 </script>
 
 <template>
+  <button @click="init">1455</button>
   <CurrencyPicker v-model:value="form.currency" :labelShowName="false" />
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import CurrencyPicker from '../dist/index.mjs' //导入
-import '../dist/style.css'
-// import CurrencyPicker from './components/picker' //导入
+// import CurrencyPicker from '../dist/index.mjs' //导入
+// import '../dist/style.css'
+import CurrencyPicker from './components/picker' //导入
 
 const app = createApp(App)
 
